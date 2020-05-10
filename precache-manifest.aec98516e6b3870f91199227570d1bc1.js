c9544a6d0d4a320b57085a69cd25e6fbb51367f9
Pablo CABRERA PEREZ <pablo.cabrera@alten.es>
2020-05-10 17:09:44 +0200

    Updates

---
+++ precache-manifest.aec98516e6b3870f91199227570d1bc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f467086850c25fa6b1581707a9548cac",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "4ccac49710cd341c6fc4",
+    "url": "/react-tictactoe/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4ffbf1309f9353d0337e",
+    "url": "/react-tictactoe/static/js/2.48573e5a.chunk.js"
+  },
+  {
+    "revision": "4ccac49710cd341c6fc4",
+    "url": "/react-tictactoe/static/js/main.4d779f3e.chunk.js"
+  },
+  {
+    "revision": "32b32cc97a4c7174c6c0",
+    "url": "/react-tictactoe/static/js/runtime~main.f8e643ec.js"
+  }
+]);
